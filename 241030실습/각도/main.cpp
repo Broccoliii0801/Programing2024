587e0b0e91c5369b1c701870afbae60a699ded89
Heejun Yang <didgmlwns56@gmail.com>
2024-10-30 17:10:15 +0900

    각도

--- 241030실습/각도/main.cpp
+++ 241030실습/각도/main.cpp
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include <math.h>
+#include <math.h>    //sin, cos 함수 사용을 위해 사용
 
 #define PI 3.141592
 
 double degtorad(double degree)
 {
-	return degree * (PI / 180.0);
+	return degree * (PI / 180.0);   //각도를 라디안으로 변환
 }
 
 int main()
@@ -14,13 +14,13 @@ int main()
 	printf("각도를 입력하세요(0-180): \n");
 	scanf_s("%lf", &degree);
 
-	if (degree < 0 || degree > 180)
+	if (degree < 0 || degree > 180)    //각도 범위 확인
 	{
 		printf("잘못된 값입니다.\n");
 		return 1;
 	}
 
-	calsin = sin(degtorad(degree));
+	calsin = sin(degtorad(degree));    /sin, cos 값 계산
 	calcos = cos(degtorad(degree));
 
 	printf("각도: %.0lf, sin: %.4lf, cos: %.4lf\n", degree, calsin, calcos);
