80cfb00dc5fc49e2c4b0f9f678b933cdc2d87e7b
Heejun Yang <didgmlwns56@gmail.com>
2024-12-04 18:06:11 +0900

    2

--- 241204실습/2/main.cpp
+++ 241204실습/2/main.cpp
@@ -2,47 +2,47 @@
 #include <stdlib.h>
 #include <time.h>
 
-void GenerateArray(float *arr, int size)
+void GenerateArray(float* arr, int size) // 포인터로 배열 생성
 {
     for (int i = 0; i < size; i++)
     {
-        arr[i] = (rand() % 2001 - 1000) / 10.0; // -100.0 ~ +100.0 범위의 랜덤 값 생성
+        *(arr + i) = (rand() % 2001 - 1000) / 10.0; // 포인터로 배열 요소 접근
     }
 }
 
-void PrintArray(float *arr, int size)          // 배열 출력 함수
+void PrintArray(float* arr, int size) // 포인터로 배열 출력
 {
     for (int i = 0; i < size; i++)
     {
-        printf("%.1f ", arr[i]);
+        printf("%.1f ", *(arr + i)); // 포인터로 배열 요소 출력
     }
     printf("\n");
 }
 
-void CalcInverseArray(float *arr, int size)       // 배열을 역순으로 바꿔주는 함수
+void CalcInverseArray(float* arr, int size) // 포인터로 배열 역순 변환
 {
     for (int i = 0; i < size / 2; i++)
     {
-        float temp = arr[i];
-        arr[i] = arr[size - 1 - i];
-        arr[size - 1 - i] = temp;
+        float temp = *(arr + i); // 포인터로 요소 접근
+        *(arr + i) = *(arr + size - 1 - i);
+        *(arr + size - 1 - i) = temp;
     }
 }
 
 int main()
 {
-    float arr[10];
-    srand(time(0)); // 배열 생성 초기화
+    float arr[10]; // 배열 선언
+    srand(time(0)); // 랜덤 초기화
 
-    GenerateArray(arr, 10);  // 배열 생성
+    GenerateArray(arr, 10); // 배열 생성 (포인터 사용)
 
-    printf("생성된 배열:\n");    // 생성된 배열 출력
-    PrintArray(arr, 10);
+    printf("생성된 배열:\n");
+    PrintArray(arr, 10); // 생성된 배열 출력 (포인터 사용)
 
-    CalcInverseArray(arr, 10);   // 배열 역순 변환
+    CalcInverseArray(arr, 10); // 배열 역순 변환 (포인터 사용)
 
-    printf("역순 배열:\n");     // 역순 배열 출력
-    PrintArray(arr, 10);
+    printf("역순 배열:\n");
+    PrintArray(arr, 10); // 역순 배열 출력 (포인터 사용)
 
     return 0;
 }
